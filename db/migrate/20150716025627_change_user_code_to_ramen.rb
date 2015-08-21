06c9106424c33397ac0802aa06edf127a235c868
kanahebi <hebibibibeam1109@gmail.com>
2015-08-21 17:32:58 +0900

    fix migrate

--- db/migrate/20150716025627_change_user_code_to_ramen.rb
+++ db/migrate/20150716025627_change_user_code_to_ramen.rb
@@ -1,6 +1,6 @@
 class ChangeUserCodeToRamen < ActiveRecord::Migration
   def up
-    change_column :ramen, :user_code, :integer, null: false
+    change_column :ramen, :user_code, 'integer USING CAST(column_name AS integer)', null: false
   end
 
   def down
